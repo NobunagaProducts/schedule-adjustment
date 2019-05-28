e1af76400aa4c063d1462113b7695775fd7b7ee2
Nobunaga <51013852+NobunagaProducts@users.noreply.github.com>
2019-05-28 19:37:37 +0900

    Add: コントローラー追加。
    イベント内容を追加、詳細情報表示、編集画面表示、編集、削除機能追加の枠だけ作成。

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Event;
+
+class EventController extends Controller
+{
+    
+    public function create(Request $request)
+    {
+    }
+    
+    public function showInfo()
+    {
+    }
+    
+    public function showEditInfo()
+    {
+    }
+    
+    public function edit()
+    {
+    }
+    
+    public function delete()
+    {
+    }
+    
+    
+}
