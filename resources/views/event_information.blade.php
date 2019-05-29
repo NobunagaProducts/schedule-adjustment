082cecacc2e9c9c6358697088fc4110e7f01f340
Nobunaga <51013852+NobunagaProducts@users.noreply.github.com>
2019-05-30 02:28:50 +0900

    Update: イベント情報画面に編集ボタンを追加。

--- resources/views/event_information.blade.php
+++ resources/views/event_information.blade.php
@@ -1,4 +1,8 @@
-<h1>イベント名:{{$event_name}}</h1>
-<h1>詳細:{{$detail}}</h1>
-<h1>イベント作成日:{{$created_at}}</h1>
-<h1>候補日:{{$possible_date}}</h1>
+<form action="/event/edit" method="get">
+    <h1>イベント名:{{$event_name}}</h1>
+    <h1>詳細:{{$detail}}</h1>
+    <h1>イベント作成日:{{$created_at}}</h1>
+    <h1>候補日:{{$possible_date}}</h1>
+
+    <button type="submit">編集</button>
+</form>
