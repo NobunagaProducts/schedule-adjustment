21d7cc07ca5a7295dd3b800bb24390c9af17951b
Nobunaga <51013852+NobunagaProducts@users.noreply.github.com>
2019-06-02 19:03:38 +0900

    Update: イベント情報表示で候補日を複数表示できるよう変更

--- resources/views/event_information.blade.php
+++ resources/views/event_information.blade.php
@@ -2,7 +2,9 @@
     <h1>イベント名:{{$event_name}}</h1>
     <h1>詳細:{{$detail}}</h1>
     <h1>イベント作成日:{{$created_at}}</h1>
-    <h1>候補日:{{$possible_date}}</h1>
-
+    <h1>候補日</h1>
+    @foreach($possible_dates as $date)
+        <h2>{{$date->possible_dates}}</h2>
+    @endforeach
     <button type="submit">編集</button>
 </form>
