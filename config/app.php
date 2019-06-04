b259681910cfe7c82bdc437a57358affc917d9e7
Nobunaga <51013852+NobunagaProducts@users.noreply.github.com>
2019-06-04 20:07:23 +0900

    サービスの試験導入 (#7)
    * Add: イベントに関する操作をサービスコンテナ化
    
    - EventController.phpに書いてあるイベント関係の処理をまとめた。
    - 例外メッセージを変更。
    - メソッド化に伴い処理の一部を変更。
    
    * Delete: ファイル名が微妙だったので削除。
    
    ManagingEvent->Partyに変更。
    
    * Add: Partyのサービスコンテナ、プロバイダー、ファサードを追加。
    
    Partyのサービスが完成。
    
    * Update: Partyのサービスを呼べるようにした。
    
    Partyサービスコンテナをプロバイダー、ファサードで呼び出せるように設定。
    
    * Update: EventControllerの一部をサービスに置き換え。
    
    EventControllerのイベント作成機能をサービスに置き換え。

--- config/app.php
+++ config/app.php
@@ -174,6 +174,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        /*
+         * Business Logic Service Providers...
+         */
+        App\Providers\PartyServiceProvider::class,
 
     ],
 
@@ -225,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Party' => App\Service\Party::class,
     ],
 
 ];
