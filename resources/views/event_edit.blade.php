36837ef6bc1ddd190eda79e8eaf7baa835248ba0
Nobunaga <51013852+NobunagaProducts@users.noreply.github.com>
2019-06-05 22:15:47 +0900

    Update: イベント編集画面で候補日を複数表示できるようにした。
    - 今までと同じHTML配列？を使用。

--- resources/views/event_edit.blade.php
+++ resources/views/event_edit.blade.php
@@ -4,7 +4,7 @@
     <h1>詳細:<input type="text" name="detail" value="{{$detail}}"></h1>
     <h1>候補日:</h1>
     @foreach($possible_dates as $date)
-        <input type="datetime-local" name="possible_date" value="{{$date}}">
+        <input type="datetime-local" name="possible_dates[]" value="{{$date}}">
     @endforeach
     <button type="submit">編集</button>
 </form>
