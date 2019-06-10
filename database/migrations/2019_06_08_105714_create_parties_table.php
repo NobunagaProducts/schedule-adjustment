df52fc2127a7a77777545ae3396944162dd8db84
Nobunaga <51013852+NobunagaProducts@users.noreply.github.com>
2019-06-10 21:08:09 +0900

    Add: partiesテーブル、モデル追加

---
+++ database/migrations/2019_06_08_105714_create_parties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePartiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parties', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('hash_value');
+            $table->string('event_name');
+            $table->string('detail');
+            $table->string('author')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parties');
+    }
+}
